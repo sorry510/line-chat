e0b22a25d36f702347758fabd6565101e54cc7c1
sorry510 <491559675@qq.com>
2022-03-01 20:41:20 +0800

    init

---
+++ demo.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . './vendor/autoload.php';
+
+$token = 'hG0tAxNAC1PgRQ01ctOKgey62qndAITr4DJQsWK5TVkY6r3TOixd7JIgJwzHUvFEED66ebv8s60d/a9lxuAFRAbUdzsIUiSArMD4Hsh1Wa9rI0Ai3ZtrpwivcdUtiz3vdLahj6zTG/SrplObFojZuwdB04t89/1O/w1cDnyilFU=';
+$secret = '65369c071154b7ed39dec0591b220927';
+$uid = 'U34a89770e846f5205ce6e786b6bf3895';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $secret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->pushMessage($uid, $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
